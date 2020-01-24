b754a58f31c2ea6d0b85478cf071ce3963d32d29
mateustech <mateustech686@gmail.com>
2020-01-23 22:50:21 -0300

    heroku - PORTA

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const app = require('./server');
 const routes = require('./routes');
 
 app.use(routes);
-const PORTA = process.env.POT || 3333
+const PORTA = process.env.PORT || 3333
 app.listen(PORTA, () => {
   console.log(`APi rodando na porta ${PORTA}`);
 });
