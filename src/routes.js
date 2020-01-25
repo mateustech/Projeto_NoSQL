493112a34861e94e517ae47b68433ca0a18750a7
mateustech <mateustech686@gmail.com>
2020-01-25 11:20:54 -0300

    Rota de disponibilidade da api

--- src/routes.js
+++ src/routes.js
@@ -7,7 +7,7 @@ const Agriesttt = require('./controllers/agriesttt')
 
 //TODO: ROTA PARA TESTE DE DISPONIBILIDADE DA API
 routes.get('/', (req, res) => {
-  return res.send('<h1>Ola, Esta API Esta Funcionando Com Sucesso!</h1>')
+  return res.status(200).send('<h1>Ola, Esta API Esta Funcionando Com Sucesso!</h1>')
 })
 //TODO:Dados Permanentes
 routes.get('/areacol/brpm', Brpm.areacol)
